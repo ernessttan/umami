5e7634e1bc902a9c0732a6b1c6ed5e3def0c9851
Ernest <ernesttancw@gmail.com>
2022-06-25 19:25:38 -0400

    Added auth context provider

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { lazy, Suspense } from 'react';
 import * as ROUTES from './constants/routes';
+import AuthContextProvider from './context/AuthContextProvider';
 
 const Welcome = lazy(() => import('./pages/Welcome'));
 const SignUp = lazy(() => import('./pages/SignUp'));
@@ -8,16 +9,18 @@ const Login = lazy(() => import('./pages/Login'));
 
 function App() {
   return (
-    <Router>
-      <Suspense fallback={<p>Loading...</p>}>
-        <Routes>
-          <Route path={ROUTES.WELCOME} element={<Welcome />} />
-          <Route path={ROUTES.SIGN_UP} element={<SignUp />} />
-          <Route path={ROUTES.LOG_IN} element={<Login />} />
-        </Routes>
-      </Suspense>
+    <AuthContextProvider>
+      <Router>
+        <Suspense fallback={<p>Loading...</p>}>
+          <Routes>
+            <Route path={ROUTES.WELCOME} element={<Welcome />} />
+            <Route path={ROUTES.SIGN_UP} element={<SignUp />} />
+            <Route path={ROUTES.LOG_IN} element={<Login />} />
+          </Routes>
+        </Suspense>
+      </Router>
+    </AuthContextProvider>
 
-    </Router>
   );
 }
 
