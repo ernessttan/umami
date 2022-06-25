2c07faa42aa31361d8eb473371c202682daf1eb9
Ernest <ernesttancw@gmail.com>
2022-06-24 23:19:46 -0400

    Completed UI of welcome page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { lazy, Suspense } from 'react';
+import * as ROUTES from './constants/routes';
+
+const Welcome = lazy(() => import('./pages/Welcome'));
+const SignUp = lazy(() => import('./pages/SignUp'));
+
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
+    <Router>
+      <Suspense fallback={<p>Loading...</p>}>
+        <Routes>
+          <Route path={ROUTES.WELCOME} element={<Welcome />} />
+          <Route path={ROUTES.SIGN_UP} element={<SignUp />} />
+        </Routes>
+      </Suspense>
+
+    </Router>
   );
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,7 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
-  content: [
-    './src/**/*.{js,jsx,ts,tsx}',
-  ],
+  content: ['./src/pages/**/*.{html,js}', './src/components/**/*.{html,js}', './public/index.html'],
   theme: {
     extend: {
       colors: {
