f0c6e123d41abb009a0fa0c303a42dbe2a557735
Ernest <ernesttancw@gmail.com>
2022-07-02 12:35:15 -0400

    Updated app header component to use a link

--- src/components/common/AppHeader.js
+++ src/components/common/AppHeader.js
@@ -1,17 +1,13 @@
-// import { Link } from 'react-router-dom';
-// import * as ROUTES from '../../constants/routes';
-import { useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import * as ROUTES from '../../constants/routes';
 
 function AppHeader() {
-  const routeHome = () => {
-    useNavigate('/feed');
-  };
   return (
     <header className="p-5 text-orange-500 md:shadow">
       <div className="flex">
-        <button onClick={routeHome} type="button">
+        <Link to={ROUTES.FEED}>
           <img className="h-8 w-8" src="/logo.svg" alt="Umami Icon" />
-        </button>
+        </Link>
       </div>
     </header>
   );
