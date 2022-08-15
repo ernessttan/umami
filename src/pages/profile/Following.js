ab598c092935ff801ce8a213bfe0e0c99dc93d94
Ernest Tan <69143731+ernessttan@users.noreply.github.com>
2022-08-15 19:07:04 -0400

    Added height to followers and following modal

--- src/pages/profile/Following.js
+++ src/pages/profile/Following.js
@@ -27,7 +27,7 @@ function Following({ following, toggleFollowing, isModalOpen }) {
       isOpen={isModalOpen}
       appElement={document.getElementById('root')}
       onRequestClose={toggleFollowing}
-      className="z-10 max-w-lg h-52 mx-5 bg-white mt-24 border rounded-lg shadow-xl md:mx-auto border-grey-300"
+      className="z-10 max-w-lg h-72 mx-5 bg-white mt-24 border rounded-lg shadow-xl md:mx-auto border-grey-300"
     >
       <div className="h-full flex flex-col">
         <div className="flex items-center justify-between p-3 border-b-1 border-grey-100">
